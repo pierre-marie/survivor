676dae676bda04b42c63ad3308eec5052adeb904
pierre-marie <pierre.marie.de.jaureguiberry@gmail.com>
2014-03-26 17:02:38 +0100

    @NO

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 
 var players = {};
 var playersUpdated = false;
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
-
-server.listen(80);
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+var io = require('socket.io').listen(80);
 
 io.sockets.on('connection', function (socket) {
 
