9c896559838aed2f4521ebc1c3f4369aea1cc0c1
pierre-marie <pierre.marie.de.jaureguiberry@gmail.com>
2014-03-27 21:45:42 +0100

    shoot

---
+++ player.js
@@ -0,0 +1,8 @@
+
+function Player(_x, _y, _r, _idPlayer) {
+	
+	this.x = _x;
+	this.y = _y;
+	this.r = _r;
+	this.idPlayer = _idPlayer;
+}
---
+++ shoot.js
@@ -0,0 +1,9 @@
+
+function Shoot(_x, _y, _r, _idPlayer, _uniqId) {
+	
+	this.x = _x;
+	this.y = _y;
+	this.r = _r;
+	this.idPlayer = _idPlayer;
+	this.uniqId = _uniqId;
+}
