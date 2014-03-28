cd1e06bf443920c391c7c24fdd8b53cf93c7e41f
pierre-marie <pierre.marie.de.jaureguiberry@gmail.com>
2014-03-28 15:57:17 +0100

    asd

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-
-var players = {};
-var playersUpdated = false;
-var io = require('socket.io').listen(8081);
+//DEBUG// var io = require('socket.io').listen(8081);
+var io = require('socket.io').listen(8081, { log: false });
 
 io.sockets.on('connection', function (socket) {
 
@@ -10,21 +8,10 @@ io.sockets.on('connection', function (socket) {
 	socket.emit('yourId', socket.id);
 
 	socket.on('message', function(message) {
-        console.log(socket.id + ' a envoye : ' + message);
-        
+
         if (message == 'SHOOT') {
         	socket.broadcast.emit('INCOMING_SHOOT', socket.id);
         }
-
-		if (typeof players[socket.id] == 'undefined') {
-			players[socket.id] = {};
-		}
-		if (typeof players[socket.id]['x'] == 'undefined') {
-			players[socket.id]['x'] = 0.0;
-			players[socket.id]['y'] = 0.0;
-			players[socket.id]['r'] = 0.0;
-		}
-
     });
 
 	//Receive an updated player to broadcast
@@ -33,41 +20,8 @@ io.sockets.on('connection', function (socket) {
 		socket.emit('UPDATE_PLAYER', p);
 	});
 
-	socket.on('keyboardPress', function(keys){
-		if (keys.right) {
-			players[socket.id]['right'] = true;
-		}
-		if (keys.left) {
-			players[socket.id]['left'] = true;
-		}
-		if (keys.up) {
-			players[socket.id]['up'] = true;
-		}
-		if (keys.down) {
-			players[socket.id]['down'] = true;
-		}
-		playersUpdated = true;
-	});
-	
-	socket.on('keyboardRelease', function(keys){
-		if (keys.right != true) {
-			players[socket.id]['right'] = false;
-		}
-		if (keys.left != true) {
-			players[socket.id]['left'] = false;
-		}
-		if (keys.up != true) {
-			players[socket.id]['up'] = false;
-		}
-		if (keys.down != true) {
-			players[socket.id]['down'] = false;
-		}
-		playersUpdated = true;
-	});
-
   	socket.on('disconnect', function () { 
   		console.log(socket.id + 'diconnect');
-  		delete players[socket.id];
   		socket.broadcast.emit('deco', socket.id);
   	});
 });
