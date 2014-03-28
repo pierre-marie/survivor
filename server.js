1117a2c9c2b02e52772d8b039a1236c7c8988e84
pierre-marie <pierre.marie.de.jaureguiberry@gmail.com>
2014-03-28 17:11:18 +0100

    deco

--- server.js
+++ server.js
@@ -23,5 +23,6 @@ io.sockets.on('connection', function (socket) {
   	socket.on('disconnect', function () { 
   		console.log(socket.id + ' disconnect');
   		socket.broadcast.emit('DECO', socket.id);
+  		socket.emit('DECO', socket.id);
   	});
 });
