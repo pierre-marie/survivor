56b270579d1735990dc0b653d2f8974525bb2350
pierre-marie <pierre.marie.de.jaureguiberry@gmail.com>
2014-03-31 21:45:00 +0200

    poils

--- shoot.js
+++ shoot.js
@@ -1,8 +1,8 @@
 
 function Shoot(_x, _y, _r, _idPlayer, _uniqId) {
 	
-	this.x = _x + (80 * Math.cos(_r));
-	this.y = _y + (80 * Math.sin(_r));
+	this.x = _x + (70 * Math.cos(_r));
+	this.y = _y + (70 * Math.sin(_r));
 	this.r = _r;
 	this.idPlayer = _idPlayer;
 	this.uniqId = _uniqId;
