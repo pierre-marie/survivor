cdb4a2c79759a215e502c4043efb8321aaa9c597
pierre-marie <pierre.marie.de.jaureguiberry@gmail.com>
2014-03-31 21:41:30 +0200

    sad

--- shoot.js
+++ shoot.js
@@ -1,8 +1,8 @@
 
 function Shoot(_x, _y, _r, _idPlayer, _uniqId) {
 	
-	this.x = _x + 10 * Math.cos(_r);
-	this.y = _y + 10 * Math.sin(_r);
+	this.x = _x + (1000 * Math.cos(_r));
+	this.y = _y + (1000 * Math.sin(_r));
 	this.r = _r;
 	this.idPlayer = _idPlayer;
 	this.uniqId = _uniqId;
