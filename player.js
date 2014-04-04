f8048cd1711e041661dee582e08ea1a76280862a
pierre-marie <pierre.marie.de.jaureguiberry@gmail.com>
2014-04-05 00:34:30 +0200

    gros changement

--- player.js
+++ player.js
@@ -2,19 +2,8 @@
 function Player(_x, _y, _r, _picture, _name, _idPlayer) {
 
 	this.timeAlive = 0;
-
-	//position in the center of the screen
-	this.x_center = _x;
-	this.y_center = _y;
-
-	//position display client side
-	this.x_display = _x;
-	this.y_display = _y;
-
-	//real position in the world
 	this.x = _x;
 	this.y = _y;
-
 	this.r = _r;
 	this.picture = _picture;
 	this.name = _name;
--- shoot.js
+++ shoot.js
@@ -1,10 +1,8 @@
 
-function Shoot(_x, _y, _x_display, _y_display, _r, _idPlayer, _uniqId) {
+function Shoot(_x, _y, _r, _idPlayer, _uniqId) {
 	
 	this.x = _x + (70 * Math.cos(_r));
 	this.y = _y + (70 * Math.sin(_r));
-	this.x_display = _x_display + (70 * Math.cos(_r));
-	this.y_display = _y_display + (70 * Math.sin(_r));
 	this.r = _r;
 	this.idPlayer = _idPlayer;
 	this.uniqId = _uniqId;
