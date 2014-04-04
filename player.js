dc95222ec8c9c334f2feadfba9143da7889cb4c2
pierre-marie <pierre.marie.de.jaureguiberry@gmail.com>
2014-04-05 01:30:20 +0200

    xy

--- player.js
+++ player.js
@@ -2,8 +2,13 @@
 function Player(_x, _y, _r, _picture, _name, _idPlayer) {
 
 	this.timeAlive = 0;
+	
 	this.x = _x;
 	this.y = _y;
+	
+	this.x_center = _x;
+	this.y_center = _y;
+	
 	this.r = _r;
 	this.picture = _picture;
 	this.name = _name;
