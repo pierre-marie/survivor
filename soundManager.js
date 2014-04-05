b1197868cc5fbfbbd1d1a7ebb52c9fb6ac0b777f
pierre-marie <pierre.marie.de.jaureguiberry@gmail.com>
2014-04-06 00:03:55 +0200

    bite

--- soundManager.js
+++ soundManager.js
@@ -1,18 +1,20 @@
 var SoundManager = function() {
 	var sm = this;
 
-	var sm.s_background = new Audio();;
-	var sm.s_shoot = new Audio();;
+	var sm.s_background;
+	var sm.s_shoot = new Audio();
 
 	sm.load = function() {
+		this.s_background = = new Audio();
 		var src1  = document.createElement("source");
 		src1.type = "audio/mpeg";
 		src1.src  = "./sounds/background.mp3";
-		s_background.appendChild(src1);
+		this.s_background.appendChild(src1);
 
+		this.s_shoot = = new Audio();
 		var src2  = document.createElement("source");
 		src2.type = "audio/mpeg";
 		src2.src  = "./sounds/shoot.mp3";
-		s_shoot.appendChild(src2);
+		this.s_shoot.appendChild(src2);
 	};
 }
